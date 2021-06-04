171e5ea0085ae65a32a8556150d55128283e2e6e
sprabhu5 <58062934+sprabhu5@users.noreply.github.com>
2021-06-04 11:52:51 -0400

    commit_a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const[cart, setCart]= useState(["shirt"])
+  const[items, setItems]= useState(["Shoes","Pants"]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+   <h1>inventory</h1>
+  
+   {items.map((item, i) => {return <div><button onClick = {() => setCart(arr => [...arr,item], setItems(curritems => curritems.filter( (it,j)=> j!==i )))}>Add to the cart</button>{item}</div>})}
+   <h1>Cart</h1>
+   {cart.map((item, i) => {return <div><button onClick = {() => setItems(arr => [...arr,item], setCart(curritems => curritems.filter( (it,j)=> j!==i )))}>Remove</button>{item}</div>})}
+   </div>
+
   );
 }
+
+
 
 export default App;
